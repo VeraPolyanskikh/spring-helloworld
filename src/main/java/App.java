ea76acbb1b6c1d3925802216a9b88d445b9d0fbf
VeraPolyanskikh <vpcat3@gmail.com>
2021-01-28 01:03:51 +0300

    Task 1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,17 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("Original beans instances comparison result="+(bean == bean2));
+
+        Cat cat =
+                (Cat) applicationContext.getBean("cat");
+
+        Cat cat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println("Cat beans instances comparison result="+(cat == cat2));
         System.out.println(bean.getMessage());
     }
 }
